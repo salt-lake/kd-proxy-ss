7d3390c58885a0efad47f6ba6e7a33787619c4a6
Eric <zerobxx@163.com>
2019-03-30 17:07:07 +0800

    snode use snode_udp action

--- cron/heartBeat.js
+++ cron/heartBeat.js
@@ -5,7 +5,7 @@ const {readCollected} = require('../service/readCollected')
 exports.heartBeat = new CronJob('*/15 * * * * *', async function() {
   try {
     let data = await readCollected()
-    let message = JSON.stringify(Object.assign({up:-1}, data))
+    let message = JSON.stringify(Object.assign({up:-1}, data, {action: 'snode_udp'}))
     client.send(message, err => {if (err) console.error('send udp error: ', err)})
   } catch (e) {
     console.error('readCollected error: ', e)
